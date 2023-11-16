90a67c3dd5072711f68472e8a8d7614b3136824c
Jamoladdin <jamoladdinisnatdinov2@gmail.com>
2023-11-16 21:55:25 +0500

    new upadating2

--- app.js
+++ app.js
@@ -20,16 +20,31 @@ let btn2 = document.querySelector('.select-btn2');
 let btn3 = document.querySelector('.select-btn3');
 let btn4 = document.querySelector('.select-btn4');
 
+let sentBtn = document.querySelector('.sentBtn')
+
+let sentBtn2 = document.querySelector('.sent-btn')
 
 btn1.addEventListener('click',()=>{
     individualText.innerText = 'lorem1'
+    btn1.classList.toggle('active-dark');
 });
 btn2.addEventListener('click',()=>{
     individualText.innerText = 'lorem2'
+    btn2.classList.toggle('active-dark');
 });
 btn3.addEventListener('click',()=>{
     individualText.innerText = 'lorem3'
+    btn3.classList.toggle('active-dark');
 });
 btn4.addEventListener('click',()=>{
     individualText.innerText = 'lorem4'
+    btn4.classList.toggle('active-dark');
 });
+
+sentBtn.addEventListener('click', ()=>{
+  sentBtn.classList.toggle('active-dark');
+})
+
+sentBtn2.addEventListener('click',()=>{
+  sentBtn2.classList.toggle('active-light');
+})
